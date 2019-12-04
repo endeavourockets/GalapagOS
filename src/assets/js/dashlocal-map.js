399a2e45f3743f8f7cd7618252d70d9788183a25
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2019-12-04 12:10:13 +0000

    fixed map loading problem

--- src/assets/js/dashlocal-map.js
+++ src/assets/js/dashlocal-map.js
@@ -1,20 +1,18 @@
-(function() {
-    // Register an event listener to be called when the page is loaded.
-    window.addEventListener("load", eventWindowLoaded, false);
+setTimeout(() => {
+    // Create a WorldWindow for the canvas.
+    var wwd = new WorldWind.WorldWindow("mapCanvas");
 
-    
-    // Define the event listener to initialize Web WorldWind.
-    function eventWindowLoaded() {
-        // Create a WorldWindow for the canvas.
-        var wwd = new WorldWind.WorldWindow("mapCanvas");
+    // Add some image layers to the WorldWindow's globe.
+    wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+    wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 
-        // Add some image layers to the WorldWindow's globe.
-        wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-        wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+    // Add a compass, a coordinates display and some view controls to the WorldWindow.
+    wwd.addLayer(new WorldWind.CompassLayer());
+    wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+    wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-        // Add a compass, a coordinates display and some view controls to the WorldWindow.
-        wwd.addLayer(new WorldWind.CompassLayer());
-        wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-        wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-    }
- })();
+    // remove loading symbol
+    setTimeout(() => {
+        document.querySelector('img.loading').classList.add('removed');
+    }, 500);
+},2000);
