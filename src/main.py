630db761374d0c7fd30ee9f247ac194274fefb0e
Mike <reationsmike@gmail.com>
2020-03-07 13:39:42 +0000

    added public ip add vagrant

--- src/main.py
+++ src/main.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
         {'Sensor': 'GPS', 'Altitude': 57, 'Longitude': 23, 'Latitude': 23}
     ]
     add_sensor_reading(test_array)
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
